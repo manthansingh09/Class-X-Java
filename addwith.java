c62bf84fc9856216349224061ed8c009eef0f13f
Manthan Singh <71974211+manthan111@users.noreply.github.com>
2021-11-20 13:57:58 +0530

    Add files via upload

---
+++ addwith.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class addwith
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a & b");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Sum of two numbers = "+c);        
+    }
+}
